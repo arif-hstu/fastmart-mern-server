0d9ca7207bccdf98409cc75e84c27f1309c45a7c
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-02 19:12:48 +0600

    product delete function added

--- app.js
+++ app.js
@@ -24,16 +24,20 @@ const ObjectId = require('mongodb').ObjectID;
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqnu2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-/*
+
+/***********
 *	handle database requests
-*
-*****************************************/
+************/
 // use connect method to to connect to the server
 MongoClient.connect(url, function(err, client) {
 	console.log('connection successful');
 	// define database collection
 	const productCollection = client.db(process.env.DB_NAME).collection('events');
-	// handle events data request
+
+	/***********
+	*	handle GET requests
+	************/
+	// handle products data request
 	app.get('/allProducts', (req, res) => {
 		productCollection.find({})
 			.toArray((err, documents) => {
@@ -43,6 +47,9 @@ MongoClient.connect(url, function(err, client) {
 	})
 
 
+	/***********
+	*	handle POST requests
+	************/
 	// find many with the Array of ID
 	app.post('/findProducts', (req, res) => {
 		let objectIdList = [];
@@ -57,18 +64,65 @@ MongoClient.connect(url, function(err, client) {
 			})
 	})
 
+	// handle placed orders
+	// define database collection
+	const orderCollection = client.db(process.env.DB_NAME).collection('orders');
+	// handle events data request
+	app.post('/placeOrder', (req, res) => {
+		orderCollection.insertOne(req.body)
+			.then(result => {
+				res.send(result.insertedId);
+				console.log(result, 'placeOrder');
+			})
+	})
 
-	/*
-	*
-	* handle all product post
-	*
-	*************************/
+	// handle track order post request
+	app.post('/trackOrder', (req, res) => {
+		let trackError;
+		const loggedInUser = req.body;
+		if (loggedInUser.email) {
+
+			orderCollection.find({ $and: [{ email: loggedInUser.email }] })
+				.toArray((err, documents) => {
+					res.send(documents);
+				})
+		}
+	})
+
+	// handle add product by admin
+	app.post('/addProduct', (req, res) => {
+		productCollection.insertOne(req.body)
+			.then(res => console.log(req.body));
+		res.send(JSON.stringify({
+			response: 'Your product added successfully!'
+		}));
+	})
+
+
+	// handle product deletion
+	app.post('/deleteProduct', (req, res) => {
+		console.log(req.body.deletedId)
+		productCollection.deleteOne({ "_id": ObjectId(req.body.deletedId) })
+			.then(res => console.log(res))
+			.catch(error => {
+				console.log(error)
+			})
+		res.send(JSON.stringify({
+			response: 'Your product added successfully!'
+		}));
+	})
+
+	/************
+	* handle bulk products add
+	*************/
 	// app.post('/addProducts', (req, res) => {
 	// 	productCollection.insertMany(req.body)
 	// 	.then(res=> console.log(res));
 	// 	console.log(req.body)
 	// 	res.send('your data sent to database');
 	// })
+
+
 })
 
 
